66c06f423c272aa88bbeabb17a7356b25ce11c20
Robert Veres <robert.veres@gmail.com>
2020-11-09 09:28:45 +0100

    Default idem na MainActivity Ak nie som prihlaseny posle ma na Register.kt Ak som prihlaseny, viem sa z menu Log Outnut a posle ma na Register.kt

--- app/src/main/java/com/a9ts/a9ts/MainActivity.kt
+++ app/src/main/java/com/a9ts/a9ts/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.a9ts.a9ts
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.a9ts.a9ts.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding : ActivityMainBinding
+    private lateinit var registerIntent: Intent
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        registerIntent = Intent(this, Register::class.java)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        if (auth.currentUser == null) {
+            startActivity(registerIntent)
+        } else {
+            val user = auth.currentUser?.phoneNumber.toString()
+            toast(user)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                firebaseLogout()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun firebaseLogout() {
+        auth.signOut()
+        startActivity(registerIntent)
     }
 }
--- app/src/main/java/com/a9ts/a9ts/Register.kt
+++ app/src/main/java/com/a9ts/a9ts/Register.kt
@@ -1,100 +1,110 @@
 package com.a9ts.a9ts
-
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.a9ts.a9ts.databinding.RegisterBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
+import com.google.firebase.auth.R
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import org.jetbrains.anko.toast
 import java.util.concurrent.TimeUnit
 
-
-
 class Register : AppCompatActivity() {
-
     private lateinit var auth: FirebaseAuth
     private lateinit var binding : RegisterBinding
+
     private var verificationInProgress = false
-    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     private var storedVerificationId: String? = ""
-    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
+
+
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken //nerozumiem
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var mainActivityIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        mainActivityIntent = Intent(this, MainActivity::class.java)
         binding = RegisterBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        auth = Firebase.auth
-        supportActionBar!!.title = "Your Phone Number"
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState)
+        }
 
-          binding.buttonNext.setOnClickListener {
-              val phoneNumber = binding.editTextPhoneNumber.text.toString().trim()
-              if (phoneNumber.isEmpty())
-              {
-                  binding.editTextPhoneNumber.setError("Number is required!")
-                  binding.editTextPhoneNumber.requestFocus()
-              } else {
-                  startPhoneNumberVerification(phoneNumber)
-              }
-          }
+        auth = Firebase.auth
+
+//        var userPhone = auth.currentUser?.phoneNumber.toString()
+//        toast(userPhone)
+
+        getSupportActionBar()?.setTitle("Your phone");
+
+        binding.buttonNext.setOnClickListener {
+            val phoneNumber = binding.editTextPhoneNumber.text.toString().trim()
+            val countryCode = binding.editTextCountryCode.text.toString().trim()
+
+            if (TextUtils.isEmpty(phoneNumber) || TextUtils.isEmpty(countryCode))
+            {
+                binding.editTextPhoneNumber.setError("Number is required!")
+                binding.editTextPhoneNumber.requestFocus()
+            } else {
+                val FullPhoneNumber = countryCode + phoneNumber
+                toast("Submitting $FullPhoneNumber")
+                startPhoneNumberVerification(FullPhoneNumber)
+            }
+        }
 
         binding.buttonSendCode.setOnClickListener {
-            toast("OK")
+            val code = binding.editTextVerificationCode.text.toString()
+
+            if (TextUtils.isEmpty(code)) {
+                binding.editTextVerificationCode.error = "Cannot be empty."
+            } else {
+                verifyPhoneNumberWithCode(storedVerificationId, code)
+            }
         }
 
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-//                Log.d(TAG, "onVerificationCompleted:$credential")
+                toast("onVerificationCompleted")
+
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
                 verificationInProgress = false
 
-                toast("Success!")
                 signInWithPhoneAuthCredential(credential)
             }
 
             override fun onVerificationFailed(e: FirebaseException) {
-                toast("onVerificationFailed")
-
-//                Log.w(TAG, "onVerificationFailed", e)
-                verificationInProgress = false
+                Log.w(TAG, "onVerificationFailed", e)
 
                 if (e is FirebaseAuthInvalidCredentialsException) {
-//                    Snackbar.make(
-//                        findViewById(android.R.id.content), "Ivalid phone number.",
-//                        Snackbar.LENGTH_SHORT
-//                    ).show()
-                    toast("Invalid phone number")
+                    binding.editTextPhoneNumber.error = "Invalid phone number."
                 } else if (e is FirebaseTooManyRequestsException) {
-                    toast("SMS Quota exceeded")
+                    toast("Quota exceeded")
                 }
-
-                toast("Verify failed")
+                toast("onVerificationFailed")
             }
 
-            override fun onCodeSent(verificationId: String,token: PhoneAuthProvider.ForceResendingToken) {
-//                Log.d(TAG, "onCodeSent:$verificationId")
-
+            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(TAG, "onCodeSent:$verificationId")
                 storedVerificationId = verificationId
                 resendToken = token
-
-//                toast("SMS Code sent: $verificationId")
+                toast("SMS Code sent: $verificationId")
             }
         }
-    } // onCreate
-
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
     }
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
@@ -102,42 +112,36 @@ class Register : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "signInWithCredential:success")
-                    val user = task.result?.user
-                    toast("User Logged in successfully!")
+
+                    startActivity(mainActivityIntent)
+                    toast("Signin successfull: Verification code OK")
                 } else {
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                        toast("Invalid code entered!")
+                        binding.editTextVerificationCode.error = "Invalid code."
+                        toast("Signin fail: Verification code WRONG")
                     }
-                    toast("Signin failed")
                 }
             }
     }
 
     private fun startPhoneNumberVerification(phoneNumber: String) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber, // Phone number to verify
-                60, // Timeout duration
-                TimeUnit.SECONDS, // Unit of timeout
-                this, // Activity (for callback binding)
-                callbacks) // OnVerificationStateChangedCallbacks
-
-        verificationInProgress = true
+        val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)       // Phone number to verify
+                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                .setActivity(this)                 // Activity (for callback binding)
+                .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 
-
-
-
-    private fun updateUI(user: FirebaseUser?) {
-        if (user != null) {
-//            toast("User is logged in.")
-        } else {
-//            toast("User is NOT logged in.")
-        }
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        signInWithPhoneAuthCredential(credential)
     }
 
     companion object {
-        private const val TAG = "PhoneAuthActivity"
+        private const val TAG = "PhoneAuth"
         private const val KEY_VERIFY_IN_PROGRESS = "key_verify_in_progress"
         private const val STATE_INITIALIZED = 1
         private const val STATE_VERIFY_FAILED = 3
